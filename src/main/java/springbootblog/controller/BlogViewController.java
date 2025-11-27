f6421d5da47911a09e040e6f4e5cf00d34b6bdcd
황지윤 <hjy648012@naver.com>
2025-11-27 21:30:25 +0900

    feat(view): 블로그 글 목록 화면 구현

---
+++ src/main/java/springbootblog/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package springbootblog.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import springbootblog.domain.dto.ArticleListViewResponse;
+import springbootblog.service.BlogService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
