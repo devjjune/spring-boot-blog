1274fb14ed151bfda60cbbe4894377de679eeacf
황지윤 <hjy648012@naver.com>
2025-12-03 11:12:53 +0900

    feat: 로그아웃 구현

--- src/main/java/springbootblog/controller/UserApiController.java
+++ src/main/java/springbootblog/controller/UserApiController.java
@@ -1,7 +1,12 @@
 package springbootblog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import springbootblog.domain.dto.AddUserRequest;
 import springbootblog.service.UserService;
@@ -15,5 +20,11 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원 가입 메서드 호출
         return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
