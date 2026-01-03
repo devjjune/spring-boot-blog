82ae30fac5e5d3780a65cce0023fd7a7c4f031f5
황지윤 <hjy648012@naver.com>
2026-01-03 17:10:35 +0900

    feat: OAuth2 설정 파일 작성

--- src/main/java/springbootblog/config/WebSecurityConfig.java
+++ src/main/java/springbootblog/config/WebSecurityConfig.java
@@ -1,74 +1,74 @@
-package springbootblog.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
-@RequiredArgsConstructor
-@Configuration
-public class WebSecurityConfig {
-    private final UserDetailsService userService;
-
-    // 스프링 시큐리티 기능 비활성화
-    @Bean
-    public WebSecurityCustomizer configure() {
-        return (web) -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
-    }
-
-    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        return http
-                // 인증, 인가 설정
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/signup", "/user").permitAll()
-                        .anyRequest().authenticated()
-                )
-                // 폼 기반 로그인 설정
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/articles", true)
-                )
-                // 로그아웃 설정
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                // csrf 비활성화
-                .csrf(csrf -> csrf.disable())
-
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            BCryptPasswordEncoder encoder,
-            UserDetailsService userDetailsService) {
-
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(encoder);
-        provider.setUserDetailsService(userDetailsService);
-
-        return new ProviderManager(provider);
-    }
-
-    // 패스워드 인코더로 사용할 빈 등록
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-}
+//package springbootblog.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.ProviderManager;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebSecurityConfig {
+//    private final UserDetailsService userService;
+//
+//    // 스프링 시큐리티 기능 비활성화
+//    @Bean
+//    public WebSecurityCustomizer configure() {
+//        return (web) -> web.ignoring()
+//                .requestMatchers(toH2Console())
+//                .requestMatchers("/static/**");
+//    }
+//
+//    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        return http
+//                // 인증, 인가 설정
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/login", "/signup", "/user").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                // 폼 기반 로그인 설정
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/articles", true)
+//                )
+//                // 로그아웃 설정
+//                .logout(logout -> logout
+//                        .logoutSuccessUrl("/login")
+//                        .invalidateHttpSession(true)
+//                )
+//                // csrf 비활성화
+//                .csrf(csrf -> csrf.disable())
+//
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            BCryptPasswordEncoder encoder,
+//            UserDetailsService userDetailsService) {
+//
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(encoder);
+//        provider.setUserDetailsService(userDetailsService);
+//
+//        return new ProviderManager(provider);
+//    }
+//
+//    // 패스워드 인코더로 사용할 빈 등록
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//}
