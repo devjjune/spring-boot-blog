04c0ebf1c88edc0d21e495ddc8765700922f5f5d
devjjune <jiyoon.hwang92@gmail.com>
2026-03-06 09:47:06 +0900

    chore: 학습 주석 추가

--- src/main/java/springbootblog/global/jwt/JwtProperties.java
+++ src/main/java/springbootblog/global/jwt/JwtProperties.java
@@ -5,10 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Setter
-@Getter
-@Component
-@ConfigurationProperties("jwt") // 자바 코드와 설정 파일을 연결
+@Setter // 설정값 주입 시 필요
+@Getter // 다른 클래스에서 설정값을 꺼내 쓸 때 필요
+@Component // 스프링이 관리하는 빈으로 등록
+@ConfigurationProperties("jwt") // 설정 파일에서 설정값을 찾아와 필드에 주입
 public class JwtProperties {
     private String issuer;
     private String secretKey;
